880446a60c7e7d3c922d4aec50cf69854c723d58
Choong Huh <choonghuh@gmail.com>
2015-06-06 16:14:22 -0700

    Player is randomly selected

--- hehe.py
+++ hehe.py
@@ -4,7 +4,16 @@
 from socket import *
 import sys
 import zlib
+from random import randint
+import os.path
+from subprocess import call
 
+# Initialize
+if os.path.isfile('temp.txt'):
+	call(["rm", "temp.txt"])
+if os.path.isfile('meh.html'):
+	call(["rm", "meh.html"])
+	
 # Set up socket and URL
 url="www.hoopshype.com"
 nbasock = socket(AF_INET, SOCK_STREAM)
@@ -15,15 +24,18 @@ f=open('datasrc/players.txt','r')
 htmlfd=open('meh.html','w')
 temp=open('temp.txt','wb')
 i=0
-f2.write('<!DOCTYPE html><html>')
-f2.write('<body>')
+htmlfd.write('<!DOCTYPE html><html>')
+htmlfd.write('<body>')
+whichplayer = randint(0,458)
 
-li=f.readline()[9:].rsplit('"')
+for i in range(whichplayer):
+	li=f.readline()[9:].rsplit('"')
 filename=li[0]
 imgfilename=filename[8:-4]+".jpg"
 personname=imgfilename[1:-4].rsplit('_')
 firstname=personname[0]
 lastname=personname[1]
+print("GET filename = "+filename)
 print(imgfilename)
 print("we are looking for "+firstname+" "+lastname)
 
@@ -32,8 +44,7 @@ request = "GET "+filename+" HTTP/1.1\r\n\
 Host: hoopshype.com\r\n\
 Connection: keep-alive\r\n\
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n\
-User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36\r\n\
-Accept-Language: en-US,en;q=0.8,ko;q=0.6,ja;q=0.4\r\n\r\n"
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36\r\n\Accept-Language: en-US,en;q=0.8,ko;q=0.6,ja;q=0.4\r\n\r\n"
 #Accept-Encoding: gzip, deflate, sdch\r\n\
 #omg this line ^
 
@@ -44,22 +55,23 @@ nbasock.send(brequest)		# then send to the server!
 
 while 1:
 	try:
-		nbasock.settimeout(.3)
+		nbasock.settimeout(1)
 		response=nbasock.recv(1024)
 		
-		#if response.len<6:
-		#	break
+		if len(response)<6:
+			break
 		temp.write(response)
+		#print("wrote")
 	
 	except BaseException as e:
 		print(e)
 		break
-#f2.write(li)
+#htmlfd.write(li)
 	
 f.close()
 temp.close()
 
-
+print("okay gonna look in temp.txt for "+imgfilename)
 f3=open("temp.txt",'r',encoding='UTF-8', newline='')
 while 1:
 	try:
@@ -68,6 +80,16 @@ while 1:
 			#print(what[126:])
 			pathArr=what[126:].rsplit('"')
 			path=pathArr[0]
+			break
+			
+		elif 'width="67">' in what:
+			#print(what[126:])
+			pathArr=what[126:].rsplit('"')
+			path=pathArr[0]
+			break
+		elif len(what)==0:
+			print("sorry, I don't know that guy")
+			path="none"
 			break
 	except BaseException as e:
 		print(e)
